53b47cfb730c69fbb3291ac42fc40eed3fe17a44
maronmi <michele.maroni@ext.ec.europa.eu>
2026-04-28 12:11:39 +0200

    Ex 3.1 Watchtower test

--- express-app/index.js
+++ express-app/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/about', (req, res) => {
+  res.send('<h1>Devops Test app</h1>')
+})
+
 const PORT = 8080
 
 app.listen(PORT, () => {
